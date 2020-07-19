eede67587960a3f8fc05134f194162cf9e5397ee
Ignas Maslinskas <ignasmaslinskas@gmail.com>
2020-07-19 21:56:53 +0300

    Experimental alternative auth for swashbuckle

---
+++ Startup.cs
@@ -0,0 +1,126 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
+namespace SwashAuthTest
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = false;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("KEY")),
+                    ValidateTokenReplay = false,
+                    ValidateActor = false,
+                    ValidateLifetime = false,
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                };
+            });
+
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"Example: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+                
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            PatchAuth(env);
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.InjectJavascript("/custom-auth.js");
+            });
+
+            app.UseStaticFiles();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+
+        public void PatchAuth(IWebHostEnvironment env)
+        {
+            using var authJsIn = File.OpenRead("wwwroot/custom-auth-original.js");
+            using var authJsOut = File.Open("wwwroot/custom-auth.js", FileMode.Create, FileAccess.Write, FileShare.None);
+            authJsOut.Write(Encoding.UTF8.GetBytes($"const IDENTITY_URL='{env.EnvironmentName}';{Environment.NewLine}{Environment.NewLine}"));
+            authJsIn.CopyTo(authJsOut);
+        }
+    }
+}
---
+++ wwwroot/custom-auth-original.js
@@ -0,0 +1,161 @@
+const authorize = () => {
+    // Do request or whatever.
+    console.log('Authorizing...');
+    var xUser = document.getElementById('x-username');
+    var xPass = document.getElementById('x-password');
+    var xAcc = document.getElementById('x-account');
+
+    xUser.disabled = true;
+    xPass.disabled = true;
+    xAcc.disabled = true;
+    document.getElementById('x-auth-button').disabled = true;
+
+    const user = xUser.value;
+    const pass = xPass.value;
+    const account = xAcc.value;
+    console.log(`Auth with ${user}:${pass}:${account}`);
+
+    // fetch(...) -> postAuthorize(result, error)
+    setTimeout(() => postAuthorize("smileyface", 'xd'), 1500);
+}
+
+const postAuthorize = (result, error = null) => {
+    document.getElementById('x-username').disabled = false;
+    document.getElementById('x-password').disabled = false;
+    document.getElementById('x-account').disabled = false;
+    document.getElementById('x-auth-button').disabled = false;
+
+    console.log(`Authorized with ${result}.`);
+    if (!error) {
+        window.ui.preauthorizeApiKey("Bearer", result);
+        document.getElementById('x-error-container').innerText = null;
+    } else {
+        document.getElementById('x-error-container').innerText = `Error: ${error}`;
+    }
+} 
+
+const createModal = () => {
+    const wrapper = document.getElementsByClassName('auth-wrapper');
+
+    if (wrapper.length === 0) {
+        return;
+    }
+    
+    const dialogEl = document.createElement('div');
+    dialogEl.classList.add('dialog-ux');
+
+    //#region backdrop
+    const backdropEl = document.createElement('div');
+    backdropEl.classList.add('backdrop-ux');
+    dialogEl.appendChild(backdropEl);
+    //#endregion backdrop
+    
+    //#region modal-boilerplate
+    const modalEl = document.createElement('div');
+    modalEl.classList.add('modal-ux');
+    dialogEl.appendChild(modalEl);
+    
+    const modalDialogEl = document.createElement('div');
+    modalDialogEl.classList.add('modal-dialog-ux');
+    modalEl.appendChild(modalDialogEl);
+    
+    const modalInnerEl = document.createElement('div');
+    modalInnerEl.classList.add('modal-ux-inner');
+    modalDialogEl.appendChild(modalInnerEl);
+    //#endregion modal-boilerplate
+
+    //#region modal-header
+    const modalHeaderEl = document.createElement('div');
+    modalHeaderEl.classList.add('modal-ux-header');
+    modalInnerEl.appendChild(modalHeaderEl);
+    const modalHeaderH3El = document.createElement('h3');
+    modalHeaderH3El.innerText = "Authorize using Identity service";
+    modalHeaderEl.appendChild(modalHeaderH3El);
+    //#endregion modal-header
+
+    //#region modal-body
+    const modalBodyEl = document.createElement('div');
+    modalBodyEl.classList.add('modal-ux-content');
+    modalInnerEl.appendChild(modalBodyEl);
+    
+    const modalBodyContainerEl = document.createElement('div');
+    modalBodyContainerEl.classList.add('auth-container');
+    modalBodyEl.appendChild(modalBodyContainerEl);
+    
+    const bodyTitle = document.createElement('h4');
+    bodyTitle.innerText = `To: ${IDENTITY_URL}`;
+    modalBodyContainerEl.appendChild(bodyTitle);
+
+    const errorContainer = document.createElement('h6');
+    errorContainer.id = 'x-error-container';
+    modalBodyContainerEl.appendChild(errorContainer);
+
+    const userInputContainter = document.createElement('div');
+    userInputContainter.classList.add('wrapper');
+    userInputContainter.innerHTML = '<label>Username:</label><section class=""><input id="x-username" type="text"></section>';
+    modalBodyContainerEl.appendChild(userInputContainter);
+
+    const passwordInputContainter = document.createElement('div');
+    passwordInputContainter.classList.add('wrapper');
+    passwordInputContainter.innerHTML = '<label>Password:</label><section class=""><input id="x-password" type="password"></section>';
+    modalBodyContainerEl.appendChild(passwordInputContainter);
+
+    const accountInputContainter = document.createElement('div');
+    accountInputContainter.classList.add('wrapper');
+    accountInputContainter.innerHTML = '<label>Account:</label><section class=""><input id="x-account" type="text"></section>';
+    modalBodyContainerEl.appendChild(accountInputContainter);
+
+
+    //#region modal-buttons
+    const modalButtons = document.createElement('div');
+    modalButtons.classList.add('auth-btn-wrapper');
+    modalBodyContainerEl.appendChild(modalButtons);
+    
+    const buttonAuth = document.createElement('button');
+    buttonAuth.id = "x-auth-button";
+    buttonAuth.classList.add('btn', 'modal-btn', 'auth', 'authorize', 'button');
+    buttonAuth.innerText = 'Authorize';
+    buttonAuth.onclick = (_) => authorize();
+    const buttonClose = document.createElement('button');
+    buttonClose.classList.add('btn', 'modal-btn', 'auth', 'btn-done', 'button');
+    buttonClose.innerText = 'Close';
+    buttonClose.onclick = (_) => dialogEl.remove();
+    modalButtons.appendChild(buttonAuth);
+    modalButtons.appendChild(buttonClose);
+    //#endregion modal-buttons
+    
+
+    //#endregion modal-body
+
+    wrapper[0].appendChild(dialogEl);
+}
+
+const addAuthButton = (attempt) => {
+    if (attempt > 20) {
+        return;
+    }
+    const wrapper = document.getElementsByClassName('auth-wrapper');
+    if (wrapper.length === 0) {
+        setTimeout(() => addAuthButton(attempt + 1), 250);
+        return;
+    }
+
+    const element = document.createElement('button');
+    element.classList.add('btn');
+    element.classList.add('authorize');
+    element.classList.add('unlocked');
+
+    const btnTextElement = document.createElement('span');
+    btnTextElement.innerText = "Authorize with Identity"
+    btnTextElement.style.paddingRight = 0;
+    element.appendChild(btnTextElement);
+
+    element.onclick = (e) => createModal();
+
+    wrapper[0].appendChild(element);
+}
+
+
+window.addEventListener('load', () => {
+    addAuthButton(0);
+})
